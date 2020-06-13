3041f1488549dbe0268982a615515f21c733c090
Noctstar <noctstar151712@gmail.com>
2020-06-13 13:22:29 +0900

    complete

---
+++ README.md
@@ -0,0 +1,20 @@
+# Create-Password
+
+## introduction
+
+半角英数字のパスワードを自動で生成します。  
+生成したパスワードは、 `password.txt` に保存することができます。
+
+---
+## Usage
+
+使用には、 [tkinter](https://docs.python.org/ja/3/library/tkinter.html) をインストールしている必要があります。
+
+```
+pip install tkinter
+```
+
+tkinterをインストールせずに当ツールを使い方は、 \dist\password\password.exe を起動してください。  
+しかし、保存の機能はご使用できません。
+
+
--- password.py
+++ password.py
@@ -1,55 +1,55 @@
 import tkinter as tk
+from tkinter import font
 import random
 import string
-import pyperclip
 
-password_length = 8
+output_path = f'./password.txt'
 
-# ボタンを押したときの処理
+# パスワードを生成
 def create_password():
-    # パスワードの生成
-    password = ''.join(random.choices(string.ascii_letters + string.digits, k=password_length))
+    password = ''.join(random.choices(string.ascii_letters + string.digits, 
+        k=int(password_length_box.get())))
     Result_label['text'] = password
-    pyperclip.copy(password)
+
+# テキストファイルにパスワードを保存
+def save_password():
+    with open(output_path, mode='a') as f:
+        f.write(
+            f"{title_box.get()}\n{account_box.get()}\n{Result_label['text']}\n\n"
+        )
 
 # ウィンドウの作成
 win = tk.Tk()
 win.title("パスワード生成アプリ")
-win.geometry("400x300")
+win.geometry("300x300")
+font1 = font.Font(family='メイリオ', size=10)
+
 
 # 部品を作成
-# テキスト
-text = tk.Label(win, text=u'パスワードを作成します。')
-text.pack()
-
 # タイトル
-title_label = tk.Label(win, text=u'タイトル')
+title_label = tk.Label(win, text=u'タイトル', font=font1)
 title_label.pack()
 title_box = tk.Entry(win)
 title_box.pack()
 
 # アカウント
-account_label = tk.Label(win, text=u'アカウント')
+account_label = tk.Label(win, text=u'アカウント', font=font1)
 account_label.pack()
 account_box = tk.Entry(win)
 account_box.insert(tk.END, 'example@gmail.com')
 account_box.pack()
 
 # パスワードの長さ
-password_length_label = tk.Label(win, text=u'パスワードの長さ')
+password_length_label = tk.Label(win, text=u'パスワードの長さ', font=font1)
 password_length_label.pack()
 password_length_box = tk.Entry(win)
 password_length_box.insert(tk.END, '8')
 password_length_box.pack()
 
-# 入力情報を取得
-title = title_box.get()
-account = account_box.get()
-password_length = int(password_length_box.get())
 
 # 決定ボタン
-create_password_Button = tk.Button(win, text=u'生成する')
-create_password_Button["command"] = create_password
+create_password_Button = tk.Button(win, text=u'生成する', font=font1)
+create_password_Button['command'] = create_password
 create_password_Button.pack()
 
 # 生成したパスワードを表示
@@ -57,8 +57,8 @@ Result_label = tk.Label(win, text=u'')
 Result_label.pack()
 
 # 保存ボタン
-save_Button = tk.Button(win, text=u'保存する')
-# save_Button["command"] = 
+save_Button = tk.Button(win, text=u'保存する', font=font1)
+save_Button['command'] = save_password
 save_Button.pack()
 
 
